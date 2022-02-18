dc89e33d74b60bf2f405b2d4d50e3ea455fa2b12
dnhuy4869 <dnhuy4869@gmail.com>
2022-02-18 12:17:15 +0700

    new message

--- src/components/DateDisplay.jsx
+++ src/components/DateDisplay.jsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 const DateDisplay = ({ date }) => {
     return (
         <section className="date-display">
-            My date is: {moment(date).format('YYYY-MM-DD hh:mm')}
+            is: {moment(date).format('YYYY-MM-DD hh:mm')}
         </section>
     )
 }
